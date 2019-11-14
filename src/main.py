5d3948bdacd33f2d386af2f240e27db7f2015d8d
Moor  Michael <michael.d.moor@gmail.com>
2019-11-14 11:42:27 +0100

    main writes the interpolated dataset to npy file

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from IPython import embed
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from tempfile import TemporaryFile
 
 from dataset import get_ucr_dataset
 from preprocessing import subsample
@@ -31,20 +32,26 @@ def main():
     # Parse Arguments and unpack the args:
     args = parser.parse_args()
     input_path = args.input_path # path to UCR datasets 
+    output_path = args.output_path # path to processed dataset 
     dataset_index = args.dataset # index, to currently used UCR dataset
     thres = args.thres # threshold for subsampling
     interpol = args.interpol # interpolation scheme
 
     datasets = os.listdir(input_path) #list of all available UCR datasets
-
+    dataset = datasets[dataset_index] 
     #Load current UCR dataset
-    X_train, y_train, X_test, y_test = get_ucr_dataset(input_path, datasets[dataset_index]) #'ItalyPowerDemand')
+    X_train, y_train, X_test, y_test = get_ucr_dataset(input_path, dataset) #'ItalyPowerDemand')
 
     np.random.seed(42)
     
     X_int = interpolate_dataset(X_train, thres, interpolation_type=interpol, plot_sample=4)
-    embed()
 
+    
+    file_path = os.path.join(output_path, dataset, interpol) 
+    os.makedirs(file_path)
+    file_name = os.path.join(file_path, 'X_interpolated.npy') 
+    with open(file_name, 'wb') as f:
+        np.save(f, X_int)
 
 if __name__ in "__main__":
     main()
