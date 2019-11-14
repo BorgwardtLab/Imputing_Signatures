5429096b258cbe3c9388df7fab29c8cf232c49d3
Michael Moor <michael.d.moor@gmail.com>
2019-11-14 17:04:35 +0100

    moved to ts format for UEA data for easier loading

--- src/download_data.py
+++ src/download_data.py
@@ -6,7 +6,7 @@ url = 'https://www.cs.ucr.edu/~eamonn/time_series_data_2018/UCRArchive_2018.zip'
 
 data_dir = 'data/'
 UCR_dir = data_dir + 'UCRArchive_2018'
-UEA_dir = data_dir + 'Multivariate_arff'
+UEA_dir = data_dir + 'Multivariate_ts'
 if not os.path.exists(UCR_dir): # check if UCR directory exists, if not download the UCR archive
     if not os.path.exists(data_dir):
         print('Creating data diretory..')
@@ -23,7 +23,7 @@ if not os.path.exists(UCR_dir): # check if UCR directory exists, if not download
 
 # Download UEA (multi-variate):
 if not os.path.exists(UEA_dir): # check if UEA directory exists, if not download the UEA archive
-    url = 'https://www.timeseriesclassification.com/Downloads/Archives/Multivariate2018_arff.zip' 
+    url = 'https://www.timeseriesclassification.com/Downloads/Archives/Multivariate2018_ts.zip' #'https://www.timeseriesclassification.com/Downloads/Archives/Multivariate2018_arff.zip' 
     print('Downloading UEA data..')
     r = requests.get(url)
     print(f'request ok: {r.ok}')
