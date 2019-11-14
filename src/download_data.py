f9df6a9f8b4a97f4392090c595c4778cee074eb3
Michael Moor <michael.d.moor@gmail.com>
2019-11-14 16:20:59 +0100

    updated download script to check if archives were downloaded before

--- src/download_data.py
+++ src/download_data.py
@@ -5,27 +5,31 @@ import os
 url = 'https://www.cs.ucr.edu/~eamonn/time_series_data_2018/UCRArchive_2018.zip' 
 
 data_dir = 'data/'
-if not os.path.exists(data_dir):
-    print('Creating data diretory..')
-    os.makedirs(data_dir)
-print('Data directory available..')
+UCR_dir = data_dir + 'UCRArchive_2018'
+UEA_dir = data_dir + 'Multivariate_arff'
+if not os.path.exists(UCR_dir): # check if UCR directory exists, if not download the UCR archive
+    if not os.path.exists(data_dir):
+        print('Creating data diretory..')
+        os.makedirs(data_dir)
+    print('Data directory available..')
 
-print('Downloading UCR data..')
-r = requests.get(url)
-print(f'request ok: {r.ok}')
+    print('Downloading UCR data..')
+    r = requests.get(url)
+    print(f'request ok: {r.ok}')
 
-print('Unzipping data files')
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall(data_dir, pwd=b'someone')
+    print('Unzipping data files')
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(data_dir, pwd=b'someone')
 
 # Download UEA (multi-variate):
-url = 'https://www.timeseriesclassification.com/Downloads/Archives/Multivariate2018_arff.zip' 
-print('Downloading UEA data..')
-r = requests.get(url)
-print(f'request ok: {r.ok}')
+if not os.path.exists(UEA_dir): # check if UEA directory exists, if not download the UEA archive
+    url = 'https://www.timeseriesclassification.com/Downloads/Archives/Multivariate2018_arff.zip' 
+    print('Downloading UEA data..')
+    r = requests.get(url)
+    print(f'request ok: {r.ok}')
 
-print('Unzipping data files')
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall(data_dir)
+    print('Unzipping data files')
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(data_dir)
 
 
