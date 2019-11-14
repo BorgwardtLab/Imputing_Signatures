1e5a664f74e47d8c70908ef6b0165affa4a06f78
mi92 <michael.d.moor@gmail.com>
2019-11-14 13:08:15 +0100

    added script for automatic data downloading

--- src/download_data.py
+++ src/download_data.py
@@ -1,11 +1,18 @@
-import requests, zipfile, ioi
+import requests, zipfile, io
+import os 
 
 url = 'https://www.cs.ucr.edu/~eamonn/time_series_data_2018/UCRArchive_2018.zip' 
 
 data_dir = 'data/'
 if not os.path.exists(data_dir):
-    os.makedir(data_dir)
+    print('Creating data diretory..')
+    os.makedirs(data_dir)
+print('Data directory available..')
+
+print('Downloading data..')
 r = requests.get(url)
 print(f'request ok: {r.ok}')
+
+print('Unzipping data files')
 z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall(data_dir)
+z.extractall(data_dir, pwd=b'someone')
