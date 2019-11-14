f1ec160fa5d644c962c205ab15e6ad20f724ac5b
Moor  Michael <michael.d.moor@gmail.com>
2019-11-14 16:12:25 +0100

    added UEA downloading snippet

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,7 @@
 import requests, zipfile, io
 import os 
 
+# Download UCR:
 url = 'https://www.cs.ucr.edu/~eamonn/time_series_data_2018/UCRArchive_2018.zip' 
 
 data_dir = 'data/'
@@ -9,10 +10,22 @@ if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 print('Data directory available..')
 
-print('Downloading data..')
+print('Downloading UCR data..')
 r = requests.get(url)
 print(f'request ok: {r.ok}')
 
 print('Unzipping data files')
 z = zipfile.ZipFile(io.BytesIO(r.content))
 z.extractall(data_dir, pwd=b'someone')
+
+# Download UEA (multi-variate):
+url = 'https://www.timeseriesclassification.com/Downloads/Archives/Multivariate2018_arff.zip' 
+print('Downloading UEA data..')
+r = requests.get(url)
+print(f'request ok: {r.ok}')
+
+print('Unzipping data files')
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall(data_dir)
+
+
