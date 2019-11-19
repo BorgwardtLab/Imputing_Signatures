08045da651fbc9d180547bc685c5a80ca0a6ba6e
Michael Moor <michael.d.moor@gmail.com>
2019-11-19 11:34:40 +0100

    added script to explore uea dataset in np format with ucr_uea_datasets package

---
+++ src/load_ucr_uea.py
@@ -0,0 +1,16 @@
+from IPython import embed
+import os
+from sktime.utils.load_data import load_from_tsfile_to_dataframe as load
+
+import uea_ucr_datasets
+
+#requires: EXPORT UEA_UCR_DATA_DIR=path/to/data/UEA_UCR
+
+uea_ucr_datasets.list_datasets()
+
+d = uea_ucr_datasets.Dataset('AllGestureWiimoteX', train=True) # AllGestureWiimoteX 'UWaveGestureLibrary'
+first_instance = d[0]
+instance_x, instance_y = first_instance
+
+embed()
+
