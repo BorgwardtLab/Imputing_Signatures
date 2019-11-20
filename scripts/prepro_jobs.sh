082e791687f4386a8698213d2388b0de3a3bbc39
Michael Moor <michael.d.moor@gmail.com>
2019-11-20 19:08:26 +0100

    adjusted thres range to 0-80

--- scripts/prepro_jobs.sh
+++ scripts/prepro_jobs.sh
@@ -8,7 +8,7 @@ index=$1
 #python src/main.py --dataset $index
 
 for i in $(seq 0 $index); do 
-   for j in $(seq 0 20 100); do 
+   for j in $(seq 0 20 80); do 
         #bsub "./test_script.sh $i $j" -n 5 -R "rusage[mem=4096]"; 
         #if [ ! -e outfiles/total_dtw_distances_channel_${j}_horizon_${i}_vs_horizon_0.npz ]; then
         echo submitting job $i, $j 
