8883f65dc75032738139fe48e8af0a7a00be63f1
Michael Moor <michael.d.moor@gmail.com>
2019-11-20 11:04:11 +0100

    added ts format compatibility to get_ucr_dataset function ONLY for 1D, equal length time series (for starters)

--- src/dataset.py
+++ src/dataset.py
@@ -5,10 +5,33 @@ Dataset utilities for UCR Archive 2018 (tsv format)
 '''
 
 
-import os
 import numpy as np
+import os
+from IPython import embed
 
+import uea_ucr_datasets
 
+def gather_iterator_in_arrays(iterator):
+    """ takes iterator from uea_ucr package returning tuples (x_i,y_i) 
+        and converts to array (sklearn format): 
+            X (n_samples, n_timesteps), 
+            y (n_samples,) 
+        --> only works for 1D time series of EQUAL length!
+    """
+    # unpack iterator:
+    time_series = []
+    labels = []
+    time_series_length = iterator[0][0].shape[0]
+    for x,y in iterator:
+        if x.shape[0] != time_series_length:
+            raise Exception('Current Data set of variable length, currently not implemented..')
+        time_series.append(x)
+        labels.append(y)
+    #TODO:  concatenate  
+    X = np.concatenate(time_series, axis=1).T    
+    y = np.array(labels)
+    return X,y 
+ 
 def strip_suffix(s, suffix):
     '''
     Removes a suffix from a string if the string contains it. Else, the
@@ -19,15 +42,21 @@ def strip_suffix(s, suffix):
         return s
     return s[:len(s)-len(suffix)]
 
-def get_ucr_dataset(data_dir: str, dataset_name: str):
+def get_ucr_dataset(data_dir: str, dataset_name: str, used_format='tsv'):
     '''
     Loads train and test data from a folder in which
     the UCR data sets are stored.
     '''
-
-    X_train, y_train, _ = read_ucr_data(os.path.join(data_dir, dataset_name, f'{dataset_name}_TRAIN.tsv'))
-    X_test, y_test, _ = read_ucr_data(os.path.join(data_dir, dataset_name, f'{dataset_name}_TEST.tsv'))
-
+    if used_format == 'tsv':
+        X_train, y_train, _ = read_ucr_data(os.path.join(data_dir, dataset_name, f'{dataset_name}_TRAIN.tsv'))
+        X_test, y_test, _ = read_ucr_data(os.path.join(data_dir, dataset_name, f'{dataset_name}_TEST.tsv'))
+    elif used_format == 'ts':
+        train_iterator = uea_ucr_datasets.Dataset(dataset_name, train=True)
+        test_iterator = uea_ucr_datasets.Dataset(dataset_name, train=False)
+        X_train, y_train = gather_iterator_in_arrays(train_iterator)  
+        X_test, y_test = gather_iterator_in_arrays(test_iterator)  
+    else: 
+        raise ValueError('Provided format is not among the implemented ones: [ts, tsv]')
     return X_train, y_train, X_test, y_test
 
 def read_ucr_data(filename):
