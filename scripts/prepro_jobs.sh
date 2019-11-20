4753e9f7099b19885f828bcde8e3c1252ae55d7c
Michael Moor <michael.d.moor@gmail.com>
2019-11-20 19:02:41 +0100

    added prepro job shell script for testing bsubs

---
+++ scripts/prepro_jobs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+module load python/3.6.0
+pipenv shell
+
+index=$1
+
+#python src/main.py --dataset $index
+
+for i in $(seq 0 $index); do 
+   for j in $(seq 0 20 100); do 
+        #bsub "./test_script.sh $i $j" -n 5 -R "rusage[mem=4096]"; 
+        #if [ ! -e outfiles/total_dtw_distances_channel_${j}_horizon_${i}_vs_horizon_0.npz ]; then
+        echo submitting job $i, $j 
+        bsub -n 1 -W 1:00 -R "rusage[mem=1024]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j";  
+        #fi
+    done;
+done
+
