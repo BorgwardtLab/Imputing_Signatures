d5bfa82f741896f5245a6f3720006fa769eb4102
Michael Moor <michael.d.moor@gmail.com>
2019-11-21 13:01:28 +0100

    extended shell script to linear interpol

--- scripts/prepro_jobs.sh
+++ scripts/prepro_jobs.sh
@@ -3,17 +3,19 @@
 module load python/3.6.0
 pipenv shell
 
-index=$1
+index=115
 
 #python src/main.py --dataset $index
 
-for i in $(seq 0 $index); do 
-   for j in $(seq 0 20 80); do 
-        #bsub "./test_script.sh $i $j" -n 5 -R "rusage[mem=4096]"; 
-        #if [ ! -e outfiles/total_dtw_distances_channel_${j}_horizon_${i}_vs_horizon_0.npz ]; then
-        echo submitting job $i, $j 
-        bsub -n 2 -W 8:00 -R "rusage[mem=2048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j";  
-        #fi
+for i in $(seq 0 $index); do  
+   for j in {50,70,90,95}; do #$(seq 0 20 80) 
+        for k in {GP,linear}; do
+            #bsub "./test_script.sh $i $j" -n 5 -R "rusage[mem=4096]"; 
+            #if [ ! -e outfiles/total_dtw_distances_channel_${j}_horizon_${i}_vs_horizon_0.npz ]; then
+            echo submitting job $i, $j, $k 
+            bsub -n 2 -W 8:00 -R "rusage[mem=2048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j --interpol $k";  
+            #fi
+        done;
     done;
 done
 
