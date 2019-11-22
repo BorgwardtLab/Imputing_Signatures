83d1a997636d7097d266f14732c12f072d734a32
Michael Moor <michael.d.moor@gmail.com>
2019-11-22 17:20:30 +0100

    changed prepro_jobs shell to use KEEP thresholds for updated subsampling, using less time for linear interpol

--- scripts/prepro_jobs.sh
+++ scripts/prepro_jobs.sh
@@ -3,19 +3,17 @@
 module load python/3.6.0
 pipenv shell
 
-index=115
+index=93
 
 #python src/main.py --dataset $index
 
 for i in $(seq 0 $index); do  
-   for j in {50,70,90,95}; do #$(seq 0 20 80) 
-        for k in {GP,linear}; do
-            #bsub "./test_script.sh $i $j" -n 5 -R "rusage[mem=4096]"; 
-            #if [ ! -e outfiles/total_dtw_distances_channel_${j}_horizon_${i}_vs_horizon_0.npz ]; then
-            echo submitting job $i, $j, $k 
-            bsub -n 2 -W 8:00 -R "rusage[mem=2048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j --interpol $k";  
-            #fi
-        done;
+   for j in {5,10,30,50}; do #$(seq 0 20 80) 
+        #for k in {GP,linear}; do
+            echo submitting job $i, $j
+            bsub -n 2 -W 1:00 -R "rusage[mem=1048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j --interpol linear";  
+            bsub -n 2 -W 8:00 -R "rusage[mem=4048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j --interpol GP";  
+        #done;
     done;
 done
 
