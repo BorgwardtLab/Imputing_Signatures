fb8826bc4e69f3eec5704c95e7dde295c0817e9d
Moor  Michael <michael.d.moor@gmail.com>
2019-11-27 18:06:56 +0100

    renamed preprocessing file before overwriting it

---
+++ src/prepro_utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+"""
+Preprocessing Utilities
+
+"""
+def subsample(X,y,prob=0.5):
+    """
+    Function to subsample time series (times X and values y) according to sampling probability prob
+    """
+    mask_indices = np.random.choice(len(X), int(np.ceil(len(X)*prob)), replace=False)
+    mask = sorted(mask_indices) #sorted from min to max
+    return X[mask], y[mask]
+
+
+def standardize(X_train, X_test): 
+    """
+    Standardize both train and test data with train stats (can handle NaNs with pandas)
+    Inputs: Np arrays,
+    Outputs: pd DataFrames!
+    """ 
+    #Convert to stacked df, for easier NaN handling. stacked for overall stats
+    X_tr = pd.DataFrame(X_train)
+    X_te = pd.DataFrame(X_test)
+    #Compute stats:
+    mean = X_tr.stack().mean()
+    std = X_tr.stack().std()
+    #Apply standardization:
+    Z_tr = (X_tr - mean) / std
+    Z_te = (X_te - mean) / std
+    return Z_tr, Z_te 
+
+def impute(X):
+    """
+    Takes pd Dataframe (already standardized), and imputes using forward filling and then 0-imputation (of remaining NaNs)
+    """ 
+    X = X.ffill()
+    X = X.fillna(0)
+    return X
+
+
