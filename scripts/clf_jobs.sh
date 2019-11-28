9533a072dfac6de37d2c5b9484385342ede29667
Michael Moor <michael.d.moor@gmail.com>
2019-11-28 16:16:16 +0100

    updated clf jobs that ran (the very most of them worked) only problem: few datasets (4) have fewer class members than n_splits of 3..

--- scripts/clf_jobs.sh
+++ scripts/clf_jobs.sh
@@ -4,17 +4,19 @@ module load python/3.6.0
 pipenv shell
 
 index=93
+thres=5 
+declare -a arr=("Sig_kNN" "Sig_LR" "Sig_LGBM" "DTW_kNN")
 
 for i in $(seq 0 $index); do  
-   for j in {5}; do #{5,10,30,50} 
-       for m in {Sig_kNN, Sig_LR, Sig_LGBM, DTW_kNN}; do 
-            echo submitting job $i, $j, $m
-            bsub -n 2 -W 2:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m --use_subsampling --thres $j --interpol GP";  
+   #for j in 5; do #{5,10,30,50} 
+       for m in "${arr[@]}"; do 
+            echo submitting job $i, $m
+            bsub -n 2 -W 2:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m --use_subsampling --thres $thres --interpol GP";  
         done;
-    done;
-    for m in {Sig_kNN, Sig_LR, Sig_LGBM, DTW_kNN}; do 
+    #done;
+    for m in "${arr[@]}"; do 
         echo submitting job without subsampling for method $m
-            bsub -n 2 -W 2:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m --interpol GP";  
+            bsub -n 2 -W 2:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m";  
         done;
 done
 
