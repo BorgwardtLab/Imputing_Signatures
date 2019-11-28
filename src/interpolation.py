b52c218c077a829b86780e536e7797491dec3e2b
Moor  Michael <michael.d.moor@gmail.com>
2019-11-28 09:38:52 +0100

    fixed minor import bug in interpolation

--- src/interpolation.py
+++ src/interpolation.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-from preprocessing import subsample
+from prepro_utils import subsample
 from scipy import interpolate
 from IPython import embed
 
