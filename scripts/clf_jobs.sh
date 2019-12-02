2a0fdc3e0cb0b249e39000f2a2cbc0a7d6abcb8c
Michael Moor <michael.d.moor@gmail.com>
2019-12-02 09:56:36 +0100

    adjusted sh script to only compute DTW as this failed due to exceeding Runtime, also updated main to preprocessing in prepro shell

--- scripts/clf_jobs.sh
+++ scripts/clf_jobs.sh
@@ -5,18 +5,19 @@ pipenv shell
 
 index=93
 thres=5 
-declare -a arr=("Sig_kNN" "Sig_LR" "Sig_LGBM" "DTW_kNN")
+#declare -a arr=("Sig_kNN" "Sig_LR" "Sig_LGBM" "DTW_kNN")
+declare -a arr=("DTW_kNN")
 
 for i in $(seq 0 $index); do  
    #for j in 5; do #{5,10,30,50} 
        for m in "${arr[@]}"; do 
             echo submitting job $i, $m
-            bsub -n 2 -W 2:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m --use_subsampling --thres $thres --interpol GP";  
+            bsub -n 4 -W 24:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m --use_subsampling --thres $thres --interpol GP";  
         done;
     #done;
     for m in "${arr[@]}"; do 
         echo submitting job without subsampling for method $m
-            bsub -n 2 -W 2:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m";  
+            bsub -n 4 -W 24:00 -R "rusage[mem=4048]" -e "clf_jobs/" -o "clf_jobs/" "python src/main.py --dataset $i --method $m";  
         done;
 done
 
--- scripts/prepro_jobs.sh
+++ scripts/prepro_jobs.sh
@@ -5,14 +5,14 @@ pipenv shell
 
 index=93
 
-#python src/main.py --dataset $index
+#python src/preprocessing.py --dataset $index
 
 for i in $(seq 0 $index); do  
    for j in {5,10,30,50}; do #$(seq 0 20 80) 
         #for k in {GP,linear}; do
             echo submitting job $i, $j
-            bsub -n 2 -W 1:00 -R "rusage[mem=1048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j --interpol linear";  
-            bsub -n 2 -W 8:00 -R "rusage[mem=4048]" -e "jobs/" -o "jobs/" "python src/main.py --dataset $i --thres $j --interpol GP";  
+            bsub -n 2 -W 1:00 -R "rusage[mem=1048]" -e "jobs/" -o "jobs/" "python src/preprocessing.py --dataset $i --thres $j --interpol linear";  
+            bsub -n 2 -W 8:00 -R "rusage[mem=4048]" -e "jobs/" -o "jobs/" "python src/preprocessing.py --dataset $i --thres $j --interpol GP";  
         #done;
     done;
 done
