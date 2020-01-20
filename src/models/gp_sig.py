2dd0861e8beb4a92ecca246cbeb3a0de83574f91
mi92 <michael.d.moor@gmail.com>
2020-01-20 20:12:47 +0100

    renamed n_tasks to n_input_dims in gp_sig class as it is more generic

--- src/models/gp_sig.py
+++ src/models/gp_sig.py
@@ -6,17 +6,17 @@ from src.models.deep_models import DeepSignatureModel
 
 
 class GP_Sig(nn.Module):
-    def __init__(self, n_tasks, n_mc_smps, n_devices, output_device, sig_depth=2):
+    def __init__(self, n_input_dims, n_mc_smps, n_devices, output_device, sig_depth=2):
         super(GP_Sig, self).__init__()
         likelihood = gpytorch.likelihoods.GaussianLikelihood().to(
                         output_device, non_blocking=True)
-        clf = DeepSignatureModel(in_channels=n_tasks, sig_depth=sig_depth)
+        clf = DeepSignatureModel(in_channels=n_input_dims, sig_depth=sig_depth)
         
         self.model = GPAdapter( clf, 
                                 None, 
                                 n_mc_smps, 
                                 likelihood, 
-                                n_tasks+1, 
+                                n_input_dims + 1, 
                                 n_devices, 
                                 output_device)
     def forward(self, *data):
