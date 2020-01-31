baa1601d767ec8e2942d2a2d5421ce93cdefaaaa
mi92 <michael.d.moor@gmail.com>
2020-01-31 19:45:43 +0100

    added two SignatureModel based GP models to the hypersearch config

--- exp/hypersearch_configs.py
+++ exp/hypersearch_configs.py
@@ -4,11 +4,36 @@ def Physionet2012():
 def add_datasets(experiment):
     experiment.named_config(Physionet2012)
 
+
+def GP_mc_SignatureModel():
+    train_module = 'train_model'
+    hyperparameter_space = {   
+        'model__parameters__sig_depth': ('Integer', 2, 4)
+    }
+    overrides = {
+        'model__name': 'GPSignatureModel',
+        'model__parameters__sampling_type': 'monte_carlo',
+        'model__parameters__n_mc_smps': 10,
+        'model__parameters__n_devices': 1,
+        'model__parameters__output_device': 'cuda'
+    }
+
+def GP_mom_SignatureModel():
+    train_module = 'train_model'
+    hyperparameter_space = {   
+        'model__parameters__sig_depth': ('Integer', 2, 4)
+    }
+    overrides = {
+        'model__name': 'GPSignatureModel',
+        'model__parameters__sampling_type': 'moments',
+        'model__parameters__n_mc_smps': 1,
+        'model__parameters__n_devices': 1,
+        'model__parameters__output_device': 'cuda'
+    }
+
+
 def GP_Sig():
     train_module = 'train_model'
-    #hyperparameter_space = {
-    #    'batch_size': ('Integer', 16, 128)
-    #}
     overrides = {
         'model__name': 'GP_Sig',
         'model__parameters__sampling_type': 'monte_carlo',
@@ -19,9 +44,6 @@ def GP_Sig():
 
 def GP_Sig_Moments():
     train_module = 'train_model'
-    #hyperparameter_space = {
-    #    'batch_size': ('Integer', 16, 128)
-    #}
     overrides = {
         'model__name': 'GP_Sig',
         'model__parameters__sampling_type': 'moments',
@@ -35,5 +57,7 @@ def GP_Sig_Moments():
 def add_models(experiment):
     experiment.named_config(GP_Sig)
     experiment.named_config(GP_Sig_Moments)
+    experiment.named_config(GP_mom_SignatureModel)
+    experiment.named_config(GP_mc_SignatureModel)
 
 
