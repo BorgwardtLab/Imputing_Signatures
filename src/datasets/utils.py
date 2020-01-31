bugfix of silent reshaping error, solved with changing the collate function in the data loader, such that tensor reshape still works

src/datasets/utils.py
@@ -19,7 +19,7 @@ DEFAULT_DATA_DIR = os.path.abspath(
 
 DATA_DIR = os.environ.get('DATA_DIR', DEFAULT_DATA_DIR)
 
-
+# Customize input transform for debuggin!
 def to_gpytorch_format(d, grid_spacing=1.0):
     """Convert dictionary with data into the gpytorch format.
 
@@ -48,6 +48,7 @@ def to_gpytorch_format(d, grid_spacing=1.0):
     min_input = np.min(inputs[:, 0])
 
     n_tasks = values.shape[-1]
+    d['n_tasks'] = n_tasks # is channel_dim
     test_inputs = np.arange(min_input, max_input + grid_spacing, grid_spacing)
     len_test_grid = len(test_inputs)
     test_inputs = np.tile(test_inputs, n_tasks)
@@ -55,7 +56,6 @@ def to_gpytorch_format(d, grid_spacing=1.0):
     d['test_inputs'] = test_inputs[:, np.newaxis].astype(np.float32)
     d['test_indices'] = test_indices[:, np.newaxis].astype(np.int64)
     return d
-
 
 def get_max_shape(l):
     """Get maximum shape for all numpy arrays in list.
@@ -70,7 +70,6 @@ def get_max_shape(l):
     shapes = np.array([el.shape for el in l])
     return np.max(shapes, axis=0)
 
-
 def dict_collate_fn(instances, padding_values=None):
     """Collate function for a list of dictionaries.
 
@@ -84,11 +83,13 @@ def dict_collate_fn(instances, padding_values=None):
         Dictionary with instances padded and combined into tensors.
 
     """
+    
     # Convert list of dicts to dict of lists
     dict_of_lists = {
         key: [d[key] for d in instances]
-        for key in instances[0].keys()
+        for key in instances[0].keys() if key != 'n_tasks'
     }
+    n_tasks = instances[0]['n_tasks']
 
     # Pad instances to max shape
     max_shapes = {
@@ -104,14 +105,45 @@ def dict_collate_fn(instances, padding_values=None):
             padding_shape = max_shape - instance_shape
             # Numpy wants the padding in the form before, after so we need to
             # prepend zeros
-            padding = np.stack(
-                [np.zeros_like(padding_shape), padding_shape], axis=1)
-            padded = np.pad(
-                instance,
-                padding,
-                mode='constant',
-                constant_values=padding_values[key]
-            )
+            
+            if 'test_' in key:
+                #for GP format test inputs, test indices, put padded/invalid points
+                #directly in the time series between the channel switches for easier reshaping later 
+                to_pad = padding_shape[0] # number of values to pad
+                if to_pad > 0: 
+                    instance_len = instance_shape[0]
+                    #temporarily get rid of extra dim (for GP format):
+                    instance = instance[:,0]
+                    time_len = int(instance_len/n_tasks) #time_length of valid observation of current instance
+                    padding = np.repeat(padding_values[key], to_pad/n_tasks) #as to_pad counts all 
+                    #padded values per multi variate time series
+                    total_padding = np.repeat(padding, n_tasks)
+                    padded_chunk_len = time_len + len(padding) #length of instance time series channel which was padded
+                    final_len = len(total_padding) + instance_len
+                    
+                    padded = np.zeros(final_len)
+                    for i in np.arange(n_tasks): # for each chunk to be padded
+                        instance_part = instance[i*time_len : (i+1)*time_len]
+                        padded_chunk = np.concatenate([instance_part, padding])
+                        padded[i*padded_chunk_len : (i+1)*padded_chunk_len] = padded_chunk              
+                    #add again the additional dim
+                    padded = padded[:, np.newaxis]
+                    if key == 'test_inputs': #make sure that format is correct
+                        padded = padded.astype(np.float32)
+                    else:
+                        padded = padded.astype(int)
+                else:
+                    padded = instance
+            else:
+                #perform standard padding
+                padding = np.stack(
+                    [np.zeros_like(padding_shape), padding_shape], axis=1)
+                padded = np.pad(
+                    instance,
+                    padding,
+                    mode='constant',
+                    constant_values=padding_values[key]
+                )
             padded_output[key].append(padded)
 
     # Combine instances into individual arrays

src/models/mgp.py
@@ -93,11 +93,9 @@ class GPAdapter(nn.Module):
 
         #reshape such that tensor has timesteps and tasks/channels in independent dimensions for Signature network:
         if self.return_gp: #useful for visualizations 
-            Z, Z_raw = self._channel_reshape(Z, test_indices, return_gp=True)
+            Z, Z_raw = self._channel_reshape(Z, return_gp=True)
         else:
-            Z = self._channel_reshape(Z, test_indices)
-
-        Z = self._collapse_tensor(Z)
+            Z = self._channel_reshape(Z)
 
         if self.return_gp:
             return self.clf(Z), Z, Z_raw
@@ -124,9 +122,9 @@ class GPAdapter(nn.Module):
         """
         mean = posterior.mean
         var = posterior.variance
-        #return torch.cat([ mean, var ], axis=-1) # concat in channel dim
-        return torch.stack([mean, var], axis=0) # stacked in a innermost dim to replace mc_smp dim
-
+        #return torch.stack([mean, var], axis=0) # stacked in a innermost dim to replace mc_smp dim
+        return torch.cat([ mean, var ], axis=-1) # concat in channel dim
+        
     def parameters(self):
         return list(self.mgp.parameters()) + list(self.clf.parameters())
 
@@ -145,46 +143,28 @@ class GPAdapter(nn.Module):
         eval simply calls eval of super class (which in turn activates train with False)
         """
         super().eval()
-    
-    def _channel_reshape(self, Z_raw, test_indices, return_gp=False):
+   
+    def _channel_reshape(self, X, return_gp=False):
         """
         reshaping function required as hadamard MGP's output format is not directly compatible with subsequent network
-        batch-wise reshaping is not super easy due to padding, for now hacky & slow approach with loops. TODO: Fix this
         """
-        channel_dim = self.n_tasks
-        stats_dim  = Z_raw.shape[0] #either mc samples or [mean/var] dimension depending on sampling type
-        batch_size = Z_raw.shape[1]
-        max_len = int(Z_raw.shape[-1] / channel_dim) # max_len of time series in the batch
-
-        #initialise resulting tensor:
-        Z = torch.zeros([stats_dim, batch_size, max_len, channel_dim])
-        for sample in torch.arange(stats_dim):
-            for i in torch.arange(batch_size):
-                Z_raw_i = Z_raw[sample, i] # GP draw array of instance i, pooling all values in single vector
-                test_indices_i = test_indices[i].flatten() # array pointing to index of each value of raw gp output
-                # Create output channels:
-                for j in torch.arange(channel_dim):
-                    z_j = Z_raw_i[test_indices_i == j] #select all (time-ordered) values that belong to task/channel j
-                    curr_len = z_j.shape[0]
-                    Z[sample, i, :curr_len, j] = z_j
-        if return_gp:
-            return Z, Z_raw
+        #first check if we have to doubele the number of channels
+        if self.sampling_type == 'moments':
+            channel_dim = 2*self.n_tasks
         else:
-            return Z
+            channel_dim = self.n_tasks
 
-    def _collapse_tensor(self, X):
-        """
-        Util function to collapse tensor depending on sampling_type
-            - mc samples are joined in batch dimension
-            - moments are stacked in channel dimension
-        In dimensions:
-            [stats_dim, batch, length, channel]
-        Out dimension:
-            [batch, length, channel]
-        """
+        X_reshaped = X.view(X.shape[:-1]                    #batch-dim (or mc_smp and batch_dim)
+            + torch.Size([channel_dim])                     #channel-dim
+            + torch.Size([int(X.shape[-1] / channel_dim)])  #time steps dim
+        )
+        # finally, swap last two dims: timestep and channel dim for Signature Augmentations
+        X_reshaped = X_reshaped.transpose(-2,-1)
+        
         if self.sampling_type == 'monte_carlo':
-            return X.flatten(0,1) # merge mc_smp and batch dim
-        elif self.sampling_type == 'moments':
-            #X = X.transpose(0,2).tranpose(0,1)
-            X = X.permute(1,2,0,3)
-            return X.flatten(-2,-1) #merge channel and moments dim
+            X_reshaped = X_reshaped.flatten(0,1) #SigNet requires 3 dim setup, so we flatten out the mc dimension with batch
+        if return_gp:
+            return X_reshaped, X
+        else: 
+            return X_reshaped
+