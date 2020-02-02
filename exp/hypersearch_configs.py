93a5753e9cabf2d1aed4776d7a24a402c86b07ae
mi92 <michael.d.moor@gmail.com>
2020-02-02 21:33:34 +0100

    added hypersearch configs of gru sig models

--- exp/hypersearch_configs.py
+++ exp/hypersearch_configs.py
@@ -8,7 +8,9 @@ def add_datasets(experiment):
 def GP_mc_SignatureModel():
     train_module = 'train_model'
     hyperparameter_space = {   
-        'model__parameters__sig_depth': ('Integer', 2, 4)
+        'model__parameters__sig_depth': ('Integer', 2, 5),
+        'model__parameters__extra_channels': ('Integer', 5, 15),
+        'model__parameters__channel_groups': ('Integer', 1, 10)
     }
     overrides = {
         'model__name': 'GPSignatureModel',
@@ -21,10 +23,46 @@ def GP_mc_SignatureModel():
 def GP_mom_SignatureModel():
     train_module = 'train_model'
     hyperparameter_space = {   
-        'model__parameters__sig_depth': ('Integer', 2, 4)
+        'model__parameters__sig_depth': ('Integer', 2, 5),
+        'model__parameters__extra_channels': ('Integer', 5, 15),
+        'model__parameters__channel_groups': ('Integer', 1, 10)
     }
     overrides = {
         'model__name': 'GPSignatureModel',
+        'model__parameters__sampling_type': 'moments',
+        'model__parameters__n_mc_smps': 1,
+        'model__parameters__n_devices': 1,
+        'model__parameters__output_device': 'cuda'
+    }
+
+def GP_mc_GRUSignatureModel():
+    train_module = 'train_model'
+    hyperparameter_space = {   
+        'model__parameters__sig_depth': ('Integer', 2, 5),
+        'model__parameters__extra_channels': ('Integer', 5, 15),
+        'model__parameters__channel_groups': ('Integer', 1, 10),
+        'model__parameters__length': ('Integer', 3, 10),
+        'model__parameters__rnn_channels': ('Categorical', [16,32,64,128]) 
+    }
+    overrides = {
+        'model__name': 'GPGRUSignatureModel',
+        'model__parameters__sampling_type': 'monte_carlo',
+        'model__parameters__n_mc_smps': 10,
+        'model__parameters__n_devices': 1,
+        'model__parameters__output_device': 'cuda'
+    }
+
+def GP_mom_GRUSignatureModel():
+    train_module = 'train_model'
+    hyperparameter_space = {   
+        'model__parameters__sig_depth': ('Integer', 2, 3),
+        'model__parameters__extra_channels': ('Integer', 5, 10),
+        'model__parameters__channel_groups': ('Integer', 1, 4),
+        'model__parameters__length': ('Integer', 3, 10),
+        'model__parameters__rnn_channels': ('Categorical', [16,32,64,128]) 
+    }
+    overrides = {
+        'model__name': 'GPGRUSignatureModel',
         'model__parameters__sampling_type': 'moments',
         'model__parameters__n_mc_smps': 1,
         'model__parameters__n_devices': 1,
@@ -59,5 +97,7 @@ def add_models(experiment):
     experiment.named_config(GP_Sig_Moments)
     experiment.named_config(GP_mom_SignatureModel)
     experiment.named_config(GP_mc_SignatureModel)
+    experiment.named_config(GP_mom_GRUSignatureModel)
+    experiment.named_config(GP_mc_GRUSignatureModel)
 
 
