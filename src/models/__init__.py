c828ed231147d1a23d872384c63b3168a404ec7d
mi92 <michael.d.moor@gmail.com>
2020-02-03 17:49:02 +0100

    completed model init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,7 @@
 """All models."""
 from .gp_sig import GPSignatureModel, GPRNNSignatureModel
 from .signature_models import SignatureModel, RNNSignatureModel
-from .imputed_models import ImputedSignatureModel
-__all__ = ['GPSignatureModel', 'GPRNNSignatureModel', 'SignatureModel', 'RNNSignatureModel', 'ImputedSignatureModel']
+from .imputed_models import ImputedSignatureModel, ImputedRNNSignatureModel, ImputedRNNModel 
+__all__ = ['GPSignatureModel', 'GPRNNSignatureModel', 'SignatureModel', 'RNNSignatureModel', 'ImputedSignatureModel',
+        'ImputedRNNSignatureModel', 'ImputedRNNModel'
+        ]
