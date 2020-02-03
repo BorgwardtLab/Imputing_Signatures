30a59fd4f62bd6b3649373adc6535df8222fd7e4
mi92 <michael.d.moor@gmail.com>
2020-02-03 01:11:48 +0100

    train module: added imputation_params config, bugfix moved train_loop before main (as sacred did not find it afterwards)

--- exp/train_model.py
+++ exp/train_model.py
@@ -31,13 +31,13 @@ def cfg():
     weight_decay = 1e-3
     early_stopping = 20
     data_format = 'GP'
-    grid_spacing = 1.  # determines n_hours between query points
-    max_root = 25  # max_root_decomposition_size for MGP lanczos iters
     device = 'cuda'
     quiet = False
     evaluation = {'active': False, 'evaluate_on': 'validation'}
-    n_mc_smps = 10 
-
+    imputation_params = {'n_mc_smps': 10, #number of monte carlo samples
+                         'max_root': 25, #max_root_decomposition_size for MGP lanczos iters
+                         'grid_spacing': 1. # determines n_hours between query points
+                        }                              
 
 @EXP.named_config
 def rep1():
@@ -69,10 +69,44 @@ class NewlineCallback(Callback):
     def on_epoch_end(self, **kwargs):
         print()
 
+def train_loop(model, dataset, data_format, loss_fn, collate_fn, n_epochs, batch_size, virtual_batch_size,
+               learning_rate, imputation_params, weight_decay=1e-5, device='cuda', callbacks=None):
+    if callbacks is None:
+        callbacks = []
+
+    virtual_scaling = 1
+    if virtual_batch_size is not None:
+        virtual_scaling = virtual_batch_size / batch_size
+
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True,
+                                               pin_memory=True, num_workers=8)
+    n_instances = len(dataset)
+    n_batches = len(train_loader)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+    epoch = 1
+    for epoch in range(1, n_epochs + 1):
+        if execute_callbacks(callbacks, 'on_epoch_begin', locals()):
+            break
+        optimizer.zero_grad()
+        for batch, d in enumerate(train_loader):
+            loss, _, _ = compute_loss(d, data_format, device, model, loss_fn, callbacks, imputation_params)
+
+            loss = loss / virtual_scaling
+            loss.backward()
+            if (batch + 1) % virtual_scaling == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+                execute_callbacks(callbacks, 'on_batch_end', locals())
+
+        if execute_callbacks(callbacks, 'on_epoch_end', locals()):
+            break
+    execute_callbacks(callbacks, 'on_train_end', locals())
 
 @EXP.automain
 def train(n_epochs, batch_size, virtual_batch_size, learning_rate, weight_decay, early_stopping, data_format,
-          grid_spacing, max_root, n_mc_smps, device, quiet, evaluation, _run, _log, _seed, _rnd):
+          imputation_params, device, quiet, evaluation, _run, _log, _seed, _rnd):
     """Sacred wrapped function to run training of model."""
 
     torch.manual_seed(_seed)
@@ -88,7 +122,7 @@ def train(n_epochs, batch_size, virtual_batch_size, learning_rate, weight_decay,
             raise ValueError(f'Virtual batch size {virtual_batch_size} has to be a multiple of batch size {batch_size}') 
     
     # Define dataset transform:
-    input_transform = get_input_transform(data_format, grid_spacing)
+    input_transform = get_input_transform(data_format, imputation_params['grid_spacing'])
 
     # Get data, sacred does some magic here so we need to hush the linter
     # pylint: disable=E1120,E1123
@@ -111,18 +145,18 @@ def train(n_epochs, batch_size, virtual_batch_size, learning_rate, weight_decay,
     # Safety guard, ensure that if mc_sampling is inactive that n_mc_smps are 1 to
     # prevent unwanted label augmentation
     if not hasattr(model, 'sampling_type'):
-        n_mc_smps = 1     
+        imputation_params['n_mc_smps'] = 1     
     elif model.sampling_type != 'monte_carlo':
-        n_mc_smps = 1
+        imputation_params['n_mc_smps'] = 1 
 
-    loss_fn = torch.nn.BCELossWithLogits()
+    loss_fn = torch.nn.BCEWithLogitsLoss()
 
     callbacks = [
         LogTrainingLoss(_run, print_progress=quiet),
-        LogDatasetLoss('validation', validation_dataset, data_format, collate_fn, loss_fn, _run, batch_size, max_root,
-                       n_mc_smps, early_stopping=early_stopping, save_path=rundir, device=device, print_progress=True),
-        LogDatasetLoss('testing', test_dataset, data_format, collate_fn, loss_fn, _run, batch_size, max_root, n_mc_smps,
-                       save_path=rundir, device=device, print_progress=False)
+        LogDatasetLoss('validation', validation_dataset, data_format, collate_fn, loss_fn, _run, imputation_params, batch_size,  
+                       early_stopping=early_stopping, save_path=rundir, device=device, print_progress=True),
+        LogDatasetLoss('testing', test_dataset, data_format, collate_fn, loss_fn, _run, imputation_params, batch_size, save_path=rundir, 
+                       device=device, print_progress=False)
     ]
     if quiet:
         # Add newlines between epochs
@@ -131,7 +165,7 @@ def train(n_epochs, batch_size, virtual_batch_size, learning_rate, weight_decay,
         callbacks.append(Progressbar())
 
     train_loop(model, train_dataset, data_format, loss_fn, collate_fn, n_epochs, batch_size, virtual_batch_size,
-               learning_rate, n_mc_smps, max_root, weight_decay, device, callbacks)
+               learning_rate, imputation_params, weight_decay, device, callbacks)
 
     if rundir:
         # Save model state (and entire model)
@@ -167,37 +201,3 @@ def train(n_epochs, batch_size, virtual_batch_size, learning_rate, weight_decay,
     return result
 
 
-def train_loop(model, dataset, data_format, loss_fn, collate_fn, n_epochs, batch_size, virtual_batch_size,
-               learning_rate, n_mc_smps=1, max_root=25, weight_decay=1e-5, device='cuda', callbacks=None):
-    if callbacks is None:
-        callbacks = []
-
-    virtual_scaling = 1
-    if virtual_batch_size is not None:
-        virtual_scaling = virtual_batch_size / batch_size
-
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True,
-                                               pin_memory=True, num_workers=8)
-    n_instances = len(dataset)
-    n_batches = len(train_loader)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-
-    epoch = 1
-    for epoch in range(1, n_epochs + 1):
-        if execute_callbacks(callbacks, 'on_epoch_begin', locals()):
-            break
-        optimizer.zero_grad()
-        for batch, d in enumerate(train_loader):
-            loss, _, _ = compute_loss(d, n_mc_smps, data_format, device, model, loss_fn, callbacks)
-
-            loss = loss / virtual_scaling
-            loss.backward()
-            if (batch + 1) % virtual_scaling == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-                execute_callbacks(callbacks, 'on_batch_end', locals())
-
-        if execute_callbacks(callbacks, 'on_epoch_end', locals()):
-            break
-    execute_callbacks(callbacks, 'on_train_end', locals())
