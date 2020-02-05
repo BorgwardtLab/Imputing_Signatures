ac35b6c8ed06eaa385a70675686508d0f4aab7ab
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-02-05 11:33:00 +0100

    Added grid for label-based subsampling

--- src/uae_preprocessing.py
+++ src/uae_preprocessing.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # Professionally specified classes :)
     datasets = [
         ('CharacterTrajectories', 20),
-        ('LSST', 14)
+        ('LSST', 14),
         ('PenDigits', 10)
     ]
 
@@ -26,19 +26,41 @@ if __name__ == '__main__':
         for t in [0.5, 0.75]:
             mar = MissingAtRandomSubsampler(t)
 
-        for name in ImputationStrategy().available_strategies:
-            transforms = [
-                    mar,
-                    ImputationStrategy(name)
-            ]
+            for name in ImputationStrategy().available_strategies:
+                transforms = [
+                        mar,
+                        ImputationStrategy(name, ensure_zero_imputation=True)
+                ]
 
-            for split in splits:
-                dataset = UEADataset(
-                            dataset_name,
-                            split,
-                            transform=transforms,
-                            use_disk_cache=True
-                )
+                for split in splits:
+                    dataset = UEADataset(
+                                dataset_name,
+                                split,
+                                transform=transforms,
+                                use_disk_cache=True
+                    )
 
-                for instance, index in enumerate(dataset):
-                    print(f'{dataset_name}: {split}: {index}')
+                    for instance, index in enumerate(dataset):
+                        print(f'MAR: {dataset_name}: {split}: {index}')
+
+    for dataset_name, n_classes in datasets:
+
+        for p_l, p_r in [(0.3, 0.6), (0.5, 0.8)]:
+            lcm = LabelBasedSubsampler(n_classes, (p_l, p_r))
+
+            for name in ImputationStrategy().available_strategies:
+                transforms = [
+                        lcm,
+                        ImputationStrategy(name, ensure_zero_imputation=True)
+                ]
+
+                for split in splits:
+                    dataset = UEADataset(
+                                dataset_name,
+                                split,
+                                transform=transforms,
+                                use_disk_cache=True
+                    )
+
+                    for index, instance in enumerate(dataset):
+                        print(f'LCM: {dataset_name}: {split}: {index}')
