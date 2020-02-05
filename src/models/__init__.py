710d69cd2e2c91a7a2cd25d150f194c72145e986
mi92 <michael.d.moor@gmail.com>
2020-02-05 02:37:36 +0100

    added deepsig to model init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,8 @@
 """All models."""
-from .gp_sig import GPSignatureModel, GPRNNSignatureModel, GPRNNModel
-from .signature_models import SignatureModel, RNNSignatureModel
-from .imputed_models import ImputedSignatureModel, ImputedRNNSignatureModel, ImputedRNNModel 
-__all__ = [ 'GPSignatureModel', 'GPRNNSignatureModel', 'GPRNNModel', 
-            'SignatureModel', 'RNNSignatureModel', 'ImputedSignatureModel',
-            'ImputedRNNSignatureModel', 'ImputedRNNModel'
+from .gp_sig import GPSignatureModel, GPRNNSignatureModel, GPRNNModel, GPDeepSignatureModel
+from .signature_models import SignatureModel, RNNSignatureModel, DeepSignatureModel
+from .imputed_models import ImputedSignatureModel, ImputedRNNSignatureModel, ImputedRNNModel, ImputedDeepSignatureModel 
+__all__ = [ 'GPSignatureModel', 'GPRNNSignatureModel', 'GPRNNModel', 'GPDeepSignatureModel', 
+            'SignatureModel', 'RNNSignatureModel', 'DeepSignatureModel', 'ImputedSignatureModel',
+            'ImputedRNNSignatureModel', 'ImputedRNNModel', 'ImputedDeepSignatureModel'
           ]
