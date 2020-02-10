44c14bc8d32a9cd734735576aa697eec3407389d
mi92 <michael.d.moor@gmail.com>
2020-02-10 09:34:19 +0100

    updated physionet train config script (instead of hypersearch)

--- scripts/create_physionet_train_configs.sh
+++ scripts/create_physionet_train_configs.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-gp_models=(GP_mc_SignatureModel GP_mom_SignatureModel GP_mc_GRUSignatureModel GP_mom_GRUSignatureModel GP_mom_GRUModel GP_mc_GRUModel GP_mom_DeepSignatureModel GP_mc_DeepSignatureModel)
-output_pattern='GP_based/experiments/train_model/{dataset}/{model}.json'
+gp_models=(model.GP_mc_SignatureModel model.GP_mom_SignatureModel model.GP_mc_GRUSignatureModel model.GP_mom_GRUSignatureModel model.GP_mom_GRUModel model.GP_mc_GRUModel)
+output_pattern='GP_based/experiments/train_model/{dataset}/{model}-{rep}.json'
 
 #GP methods:
 python scripts/configs_from_product.py exp.train_model \
+  --name rep --set rep1 rep2 rep3 rep4 \
   --name model \
   --set ${gp_models[*]} \
-  --name dataset --set Physionet2012 \
+  --name dataset --set dataset.Physionet2012 \
   --output-pattern ${output_pattern}
 
 
-imputed_models=(ImputedSignatureModel ImputedRNNSignatureModel ImputedRNNModel)
-data_formats=(zero linear forwardfill causal indicator)
-output_pattern='experiments/train_model/{dataset}/{data_format}{model}.json'
+
+gp_models=(model.GP_mom_DeepSignatureModel model.GP_mc_DeepSignatureModel)
+output_pattern='GP_based/experiments/train_model/{dataset}/{model}-{rep}.json'
 
 #GP methods:
 python scripts/configs_from_product.py exp.train_model \
+  --name rep --set rep1 rep2 rep3 rep4 \
+  --name model \
+  --set ${gp_models[*]} \
+  --name dataset --set dataset.Physionet2012 \
+  --name dummy --set model.parameters.kernel_size=1 \
+  --output-pattern ${output_pattern} 
+
+imputed_models=(model.ImputedSignatureModel model.ImputedRNNSignatureModel model.ImputedRNNModel)
+data_formats=(zero linear) #forwardfill causal indicator)
+output_pattern='experiments/train_model/{dataset}/{data_format}{model}-{rep}.json'
+
+#Imputed methods:
+python scripts/configs_from_product.py exp.train_model \
+  --name rep --set rep1 rep2 rep3 rep4 \
   --name model \
   --set ${imputed_models[*]} \
   --name dataset --set Physionet2012 \
@@ -24,7 +39,22 @@ python scripts/configs_from_product.py exp.train_model \
   --set ${data_formats[*]} \
   --output-pattern ${output_pattern}
 
+exit 
 
+imputed_models=(model.ImputedDeepSignatureModel)
+data_formats=(zero linear forwardfill causal indicator)
+output_pattern='experiments/train_model/{dataset}/{data_format}{model}-{rep}.json'
+
+#Imputed methods:
+python scripts/configs_from_product.py exp.train_model \
+  --name rep --set rep1 rep2 rep3 rep4 \
+  --name model \
+  --set ${imputed_models[*]} \
+  --name dataset --set dataset.Physionet2012 \
+  --name data_format \
+  --set ${data_formats[*]} \
+  --name dummy --set model.parameters.kernel_size=1 \
+  --output-pattern ${output_pattern}
 
 #python scripts/configs_from_product.py exp.hyperparameter_search \
 #    --name model \
