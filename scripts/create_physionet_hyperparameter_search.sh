78ff31c71bdc04ad5ed77eefb47261831801381a
mi92 <michael.d.moor@gmail.com>
2020-02-10 09:33:54 +0100

    pushing most recent version of physionet hypersearch generation script

--- scripts/create_physionet_hyperparameter_search.sh
+++ scripts/create_physionet_hyperparameter_search.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-gp_models=(GP_mc_SignatureModel GP_mom_SignatureModel GP_mc_GRUSignatureModel GP_mom_GRUSignatureModel GP_mom_GRUModel GP_mc_GRUModel GP_mom_DeepSignatureModel GP_mc_DeepSignatureModel)
-output_pattern='experiments/hyperparameter_search/GP_based/{dataset}/{model}.json'
-
-#GP methods:
-python scripts/configs_from_product.py exp.hyperparameter_search \
-  --name model \
-  --set ${gp_models[*]} \
-  --name dataset --set Physionet2012 \
-  --output-pattern ${output_pattern}
-
-
+n_runs=5
+#gp_models=(GP_mc_SignatureModel GP_mom_SignatureModel GP_mc_GRUSignatureModel GP_mom_GRUSignatureModel GP_mom_GRUModel GP_mc_GRUModel)
+#output_pattern='experiments/hyperparameter_search/{dataset}/{model}.json'
+#
+##GP methods:
+#python scripts/configs_from_product.py exp.hyperparameter_search \
+#  --name model \
+#  --set ${gp_models[*]} \
+#  --name dataset --set Physionet2012 \
+#  --output-pattern ${output_pattern}
+#
+#gp_models=(GP_mom_DeepSignatureModel GP_mc_DeepSignatureModel)
+#output_pattern='experiments/hyperparameter_search/{dataset}/{model}.json'
+#
+#
+##GP DeepSig methods:
+#python scripts/configs_from_product.py exp.hyperparameter_search \
+#  --name model \
+#  --set ${gp_models[*]} \
+#  --name dataset --set Physionet2012 \
+#  --output-pattern ${output_pattern} \
+#  --name dummy --set overrides.model__parameters__kernel_size=1
+## USE THIS LAST PART ONLY FOR GP DEEPSIG!
+#
+#
 
 imputed_models=(ImputedSignatureModel ImputedRNNSignatureModel ImputedRNNModel)
 data_formats=(zero linear forwardfill causal indicator)
-output_pattern='experiments/hyperparameter_search/{dataset}/{data_format}{model}.json'
+output_pattern='experiments/cleaned/hyperparameter_search/{dataset}/{data_format}{model}.json'
 
 #GP methods:
 python scripts/configs_from_product.py exp.hyperparameter_search \
@@ -23,8 +37,29 @@ python scripts/configs_from_product.py exp.hyperparameter_search \
   --name dataset --set Physionet2012 \
   --name data_format \
   --set ${data_formats[*]} \
-  --output-pattern ${output_pattern}
+  --output-pattern ${output_pattern} \
+  --name dummy --set n_calls=$n_runs \
+  --name dummy --set n_random_calls=$n_runs 
+  #--name n_calls --set $n_runs \
+  #--name n_random_starts --set $n_runs
 
+imputed_models=(ImputedDeepSignatureModel)
+data_formats=(zero linear forwardfill causal indicator)
+output_pattern='experiments/cleaned/hyperparameter_search/{dataset}/{data_format}{model}.json'
+
+#GP DeepSig methods:
+python scripts/configs_from_product.py exp.hyperparameter_search \
+  --name model \
+  --set ${imputed_models[*]} \
+  --name dataset --set Physionet2012 \
+  --name data_format \
+  --set ${data_formats[*]} \
+  --output-pattern ${output_pattern} \
+  --name dummy --set overrides.model__parameters__kernel_size=1 \
+  --name dummy --set n_calls=$n_runs \
+  --name dummy --set n_random_calls=$n_runs 
+
+# USE THIS LAST PART ONLY FOR GP DEEPSIG!
 
 
 #python scripts/configs_from_product.py exp.hyperparameter_search \
